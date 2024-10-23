616ad448ad2e7e7d6426bbb9fd6aa8c8ea276e7d
Harishsm20 <harishsm865@gmail.com>
2024-10-23 20:45:04 +0530

    API gateway updated

--- Feed-Backend-API-Gateway/server.js
+++ Feed-Backend-API-Gateway/server.js
@@ -1,31 +1,44 @@
+// api-gateway/server.js
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
 const bodyParser = require('body-parser');
-const app = express();
-
-const PORT = 3001;
+const cors = require('cors');
+const axios = require('axios');
 require('dotenv').config();
 
-// Connect to MongoDB
-const connect = async () => {
-  console.log('Attempting to connect to MongoDB...');
-  try {
-      await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-  }
-};
+const app = express();
+const PORT = process.env.PORT || 4000;
 
-connect();
-
-app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(cors());
 app.use(bodyParser.json());
 
+// Proxy requests based on the path
+app.use('/auth', (req, res) => {
+  axios({
+    url: `${process.env.AUTH_SERVICE_URL}${req.originalUrl}`,
+    method: req.method,
+    data: req.body,
+    headers: req.headers,
+  }).then(response => res.send(response.data)).catch(err => res.status(err.response.status).send(err.response.data));
+});
+
+app.use('/blog', (req, res) => {
+  axios({
+    url: `${process.env.BLOG_SERVICE_URL}${req.originalUrl}`,
+    method: req.method,
+    data: req.body,
+    headers: req.headers,
+  }).then(response => res.send(response.data)).catch(err => res.status(err.response.status).send(err.response.data));
+});
+
+app.use('/media', (req, res) => {
+  axios({
+    url: `${process.env.MEDIA_SERVICE_URL}${req.originalUrl}`,
+    method: req.method,
+    data: req.body,
+    headers: req.headers,
+  }).then(response => res.send(response.data)).catch(err => res.status(err.response.status).send(err.response.data));
+});
+
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`API Gateway running on port ${PORT}`);
 });
