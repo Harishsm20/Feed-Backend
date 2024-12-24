02428f2f51911d8726fdb11564c2861f33ee5312
Harishsm20 <harishsm865@gmail.com>
2024-12-24 22:51:53 +0530

    New Dependencies and files Structure changed

--- Feed-Backend-Blog-Service/routes/blogRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getAllPosts, createPost } = require('../controllers/blogController');
-const router = express.Router();
-
-router.get('/posts', getAllPosts);
-router.post('/posts', createPost);
-
-module.exports = router;
