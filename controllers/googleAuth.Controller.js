cf2229bdddb558d491ae444532d27b330ea9aa7b
Harishsm20 <harishsm865@gmail.com>
2025-01-18 20:19:10 +0530

    Google auth controller removed

--- controllers/googleAuth.Controller.js
+++
@@ -1,7 +0,0 @@
-export const getGoogleUser = (req, res) => {
-    if (req.user) {
-      res.status(200).json({ user: req.user });
-    } else {
-      res.status(401).json({ message: 'Unauthorized' });
-    }
-  };
