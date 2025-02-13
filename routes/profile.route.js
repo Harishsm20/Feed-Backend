2695cac43a5c68a35f9ce517e34232ba67b582ba
Harishsm20 <harishsm865@gmail.com>
2025-02-13 10:27:10 +0530

    Profile route updated

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -1,11 +1,18 @@
 import express from 'express';
-import { getUserFromToken, editProfile, isUserNamePresent } from '../controllers/Profile.Controller.js'; 
+import { getUserFromToken, editProfile, isUserNamePresent, getUserWithProfile, updateProfile } from '../controllers/Profile.Controller.js'; 
 
 const router = express.Router();
+const upload = multer(); // Middleware to handle file uploads
 
 // Get profile details
 router.get('/me',getUserFromToken);
 router.put('/me', editProfile)
 router.post("/check-username", isUserNamePresent);
+
+// Update profile (including profile image)
+router.put("/:userId", upload.single("profileImg"), updateProfile);
+
+// Get profile details
+router.get("/:userId", getUserWithProfile);
 
 export default router;
